b8e9111d11cd17e2785f75fa3ea97fb8a63b59b1
Tom Li <tomli113@hotmail.com>
2019-09-02 17:15:21 +1000

    update

---
+++ module-list.js
@@ -0,0 +1,15 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var modules={
+        "tom-control-panel":            {url:"$H/m/control-panel.html"},
+        "tom-knowledge-base-data":    	{url:"https://www.tomliweb.com/modules/t/tom-knowledge-base-data.html",Table:"tom-knowledge-base",form_module:"tom-knowledge-base-form",router:1},
+        "tom-knowledge-base-form":    	{url:"https://www.tomliweb.com/modules/t/tom-knowledge-base-form.html",Table:"tom-knowledge-base"},
+        
+        "module-editor":        {url:"https://vm.vmiis.com/component/m/module-editor.html",router:1},
+    }
+    for(p in modules){
+        $vm.module_list[p]=modules[p]; 
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    //-------------------------------------------------------------------------------------
+})();
